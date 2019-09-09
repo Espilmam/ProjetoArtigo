a2a9e80beaecb3abdb8a3060586103c3b31dab29
brunoarmonico <bruno.csarmonico@outlook.com>
2019-09-08 22:10:50 -0300

    Adicionado testes no chrome

--- src/main/java/tests/TesteElemento.java
+++ src/main/java/tests/TesteElemento.java
@@ -1,9 +1,10 @@
 package tests;
 
-import com.google.common.base.Stopwatch;
 import org.apache.commons.lang3.time.StopWatch;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
@@ -12,59 +13,123 @@ import org.testng.annotations.Test;
 
 public class TesteElemento {
     WebDriver driver;
+    String navegador = "firefox";
+    DesiredCapabilities capabilities;
     @BeforeClass
-    public void setUp(){
-        System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\geckodriver.exe");
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        capabilities.setCapability("marionette",true);
-        driver = new FirefoxDriver();
-        driver.get("https://outlook.live.com/owa/");
-        driver.manage().window().maximize();
+    public void setUp(String url){
+        if (navegador.contains("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\geckodriver.exe");
+//            FirefoxBinary firefoxBinary = new FirefoxBinary();
+//            firefoxBinary.addCommandLineOptions("--headless");
+//            FirefoxOptions firefoxOptions = new FirefoxOptions();
+//            firefoxOptions.setBinary(firefoxBinary);
+            capabilities = DesiredCapabilities.firefox();
+            capabilities.setCapability("marionette", true);
+            driver = new FirefoxDriver();
+            driver.get(url);
+            driver.manage().window().maximize();
+        } else {
+            System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+            ChromeOptions options = new ChromeOptions();
+//            options.addArguments("--headless");
+//            options.addArguments("--window-size=1920,1080");
+//            options.addArguments("--disable-gpu");
+//            options.addArguments("--proxy-server='direct://'");
+//            options.addArguments("--proxy-bypass-list=*");
+//            options.addArguments("--disable-extensions");
+            capabilities = DesiredCapabilities.chrome();
+            driver = new ChromeDriver(options);
+            driver.get(url);
+            driver.manage().window().maximize();
+        }
     }
 
-    StopWatch timer = new StopWatch();
+    StopWatch timerName = new StopWatch();
+    StopWatch timerID = new StopWatch();
+    StopWatch timerClassname = new StopWatch();
+    StopWatch timerClassname2 = new StopWatch();
+    StopWatch timerXpath = new StopWatch();
+    StopWatch timerLinkText = new StopWatch();
+    StopWatch timerLPartialinkText = new StopWatch();
+    StopWatch timerCss = new StopWatch();
+
 
     @Test
-    public void teste (){
+    public void testeOutlook (){
+        String url = "https://outlook.live.com/owa/";
         try {
-            Thread.sleep(8000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        timer.start();
-        driver.findElement(By.xpath("//span[contains(.,'Suporte')]"));
-        timer.stop();
-        System.out.println(timer.getTime());
+        long id = 0;
+        long xpath = 0;
+        long name = 0;
+        long classname = 0;
+        long classname2 = 0;
+        long linktext = 0;
+        long partialLinktext = 0;
+        long css = 0;
 
-        timer.reset();
+        for (int cont = 0; cont < 1000; cont ++) {
+            timerXpath.start();
+            driver.findElement(By.xpath("//span[contains(.,'Suporte')]"));
+            xpath += timerXpath.getTime();
+            timerXpath.reset();
 
-        timer.start();
-        driver.findElement(By.id("security"));
-        timer.stop();
-        System.out.println(timer.getTime());
+            timerID.start();
+            driver.findElement(By.id("security"));
+            id += timerID.getTime();
+            timerID.reset();
 
-        timer.reset();
+            timerName.start();
+            driver.findElement(By.name("productivity-apps"));
+            name += timerName.getTime();
+            timerName.reset();
 
-        timer.start();
-        driver.findElement(By.name("productivity-apps"));
-        timer.stop();
-        System.out.println(timer.getTime());
+            timerClassname.start();
+            driver.findElement(By.className("masthead"));
+            classname += timerClassname.getTime();
+            timerClassname.reset();
 
+            timerClassname2.start();
+            driver.findElement(By.className("tagline"));
+            classname2 += timerClassname2.getTime();
+            timerClassname2.reset();
 
-        timer.reset();
+            timerLinkText.start();
+            driver.findElement(By.linkText("Suporte"));
+            linktext += timerLinkText.getTime();
+            timerLinkText.reset();
 
-        timer.start();
-        driver.findElement(By.className("masthead"));
-        timer.stop();
-        System.out.println(timer.getTime());
-        timer.reset();
+            timerLPartialinkText.start();
+            driver.findElement(By.partialLinkText("Suporte"));
+            partialLinktext += timerLPartialinkText.getTime();
+            timerLPartialinkText.reset();
 
-        timer.start();
-        driver.findElement(By.linkText("Suporte"));
-        timer.stop();
-        System.out.println(timer.getTime());
+            timerCss.start();
+            driver.findElement(By.cssSelector(".masthead-shell .tagline"));
+            css += timerCss.getTime();
+            timerCss.reset();
+        }
+        System.out.println(navegador);
+
+        System.out.println(id + " - ID");
+        System.out.println(name + " - Name");
+        System.out.println(linktext + " - LinkText");
+        System.out.println(partialLinktext + " - Partial LinkText");
+        System.out.println(classname + " - Classname");
+        System.out.println(classname2 + " - Classname2");
+        System.out.println(xpath + " - X-Path");
+        System.out.println(css + " - Css Selector");
     }
 
+    @Test
+    public void testeW3c () {
+        String url = "https://www.w3.org/";
+
+
+    }
 
     @AfterMethod
     public void quit(){
