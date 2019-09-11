ec7ccfb1973084d62657e93634abe91fa53e922f
brunoarmonico <bruno.csarmonico@outlook.com>
2019-09-10 22:34:36 -0300

    Adicionado XML

--- src/main/java/tests/TesteElemento.java
+++ src/main/java/tests/TesteElemento.java
@@ -13,15 +13,36 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 
 public class TesteElemento {
     WebDriver driver;
     String navegador = "firefox";
     DesiredCapabilities capabilities;
+    long id = 0;
+    long xpath = 0;
+    long name = 0;
+    long classname = 0;
+    long linktext = 0;
+    long partialLinktext = 0;
+    long css = 0;
 
     public void setUp(String url){
+        id = 0;
+        xpath = 0;
+        name = 0;
+        classname = 0;
+        linktext = 0;
+        partialLinktext = 0;
+        css = 0;
+        System.out.println(url + " - URL");
         if (navegador.contains("firefox")) {
             System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\geckodriver.exe");
             FirefoxBinary firefoxBinary = new FirefoxBinary();
@@ -30,7 +51,7 @@ public class TesteElemento {
             firefoxOptions.setBinary(firefoxBinary);
             capabilities = DesiredCapabilities.firefox();
             capabilities.setCapability("marionette", true);
-            driver = new FirefoxDriver();
+            driver = new FirefoxDriver(firefoxOptions);
             driver.get(url);
             driver.manage().window().maximize();
         } else {
@@ -39,7 +60,7 @@ public class TesteElemento {
             options.setHeadless(true);
             options.addArguments("--disable-gpu --proxy-bypass-list=* --proxy-server='direct://'");
             capabilities = DesiredCapabilities.chrome();
-            driver = new ChromeDriver(options);
+            driver = new ChromeDriver();
             driver.get(url);
             driver.manage().window().maximize();
         }
@@ -48,7 +69,6 @@ public class TesteElemento {
     StopWatch timerName = new StopWatch();
     StopWatch timerID = new StopWatch();
     StopWatch timerClassname = new StopWatch();
-    StopWatch timerClassname2 = new StopWatch();
     StopWatch timerXpath = new StopWatch();
     StopWatch timerLinkText = new StopWatch();
     StopWatch timerLPartialinkText = new StopWatch();
@@ -56,21 +76,15 @@ public class TesteElemento {
 
 
     @Test
-    public void testeOutlook (){
+    @Parameters({"navegador"})
+    public void testeOutlook (@Optional("") String navegador){
+        this.navegador = navegador;
         setUp("https://outlook.live.com/owa/");
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        long id = 0;
-        long xpath = 0;
-        long name = 0;
-        long classname = 0;
-        long classname2 = 0;
-        long linktext = 0;
-        long partialLinktext = 0;
-        long css = 0;
 
         for (int cont = 0; cont < 1000; cont ++) {
             timerXpath.start();
@@ -92,11 +106,6 @@ public class TesteElemento {
             driver.findElement(By.className("masthead"));
             classname += timerClassname.getTime();
             timerClassname.reset();
-
-            timerClassname2.start();
-            driver.findElement(By.className("tagline"));
-            classname2 += timerClassname2.getTime();
-            timerClassname2.reset();
 
             timerLinkText.start();
             driver.findElement(By.linkText("Suporte"));
@@ -120,20 +129,93 @@ public class TesteElemento {
         System.out.println(linktext + " - LinkText");
         System.out.println(partialLinktext + " - Partial LinkText");
         System.out.println(classname + " - Classname");
-        System.out.println(classname2 + " - Classname2");
         System.out.println(xpath + " - X-Path");
         System.out.println(css + " - Css Selector");
     }
 
     @Test
-    public void testeW3c () {
-        String url = "https://www.w3.org/";
+    @Parameters({"navegador"})
+    public void testeW3c (@Optional("") String navegador) {
+        this.navegador = navegador;
+        setUp("https://www.w3.org/");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
 
+        for (int cont = 0; cont < 1000; cont ++) {
+
+            timerID.start();
+            driver.findElement(By.id("w3c_home_talks"));
+            id += timerID.getTime();
+            timerID.reset();
+
+            timerName.start();
+            driver.findElement(By.name("region"));
+            name += timerName.getTime();
+            timerName.reset();
+
+            timerClassname.start();
+            driver.findElement(By.className("w3c_upcoming_events"));
+            classname += timerClassname.getTime();
+            timerClassname.reset();
+
+            timerLinkText.start();
+            driver.findElement(By.linkText("Privacy"));
+            linktext += timerLinkText.getTime();
+            timerLinkText.reset();
+
+            timerLPartialinkText.start();
+            driver.findElement(By.partialLinkText("Privacy"));
+            partialLinktext += timerLPartialinkText.getTime();
+            timerLPartialinkText.reset();
+
+            timerCss.start();
+            driver.findElement(By.cssSelector(".w3c_mainCol .w3c_upcoming_events"));
+            css += timerCss.getTime();
+            timerCss.reset();
+
+            timerXpath.start();
+            driver.findElement(By.xpath("//h2[contains(.,'Events')]"));
+            xpath += timerXpath.getTime();
+            timerXpath.reset();
+        }
+
+        System.out.println(navegador);
+
+        System.out.println(id + " - ID");
+        System.out.println(name + " - Name");
+        System.out.println(linktext + " - LinkText");
+        System.out.println(partialLinktext + " - Partial LinkText");
+        System.out.println(classname + " - Classname");
+        System.out.println(xpath + " - X-Path");
+        System.out.println(css + " - Css Selector");
     }
 
     @AfterMethod
     public void quit(){
+        /*PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("target\\resultado.txt", "UTF-8");
+            writer.println(navegador);
+            writer.println(id);
+            writer.println(name);
+            writer.println(linktext);
+            writer.println(partialLinktext);
+            writer.println(classname);
+            writer.println(xpath);
+            writer.println(css);
+            writer.println("--------------------------------");
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }*/
+
         /*File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         try {
             // now copy the  screenshot to desired location using copyFile //method
